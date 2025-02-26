31eb46f4835983bc3d1ff5c3ba7cf4eeab89be54
zjj <2039437335@qq.com>
2025-02-26 15:18:56 +0800

    开始尝试着看一看怎么调取数据了

---
+++ data/major-detail-data.js
@@ -0,0 +1,15 @@
+// data/major-detail-data.js
+const majorStats = {
+    '40101': { // 假设40101是教育学的ID
+      employmentRate: '90%',
+      avgSalary: '5000元',
+      furtherRate: '10%',
+      schoolCount: '100所',
+      transitionRate: '5%'
+    },
+    // 其他学科的数据...
+  };
+  
+  module.exports = {
+    majorStats
+  };
